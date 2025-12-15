bacdf549f0fda6e0b513ccad572ad1e2201d3089
Ariël Hermas Yapi <arielhermas@gmail.com>
2025-12-15 22:38:11 +0000

    Ajout des fonctionnalités météo

--- gemini_api/chat/urls.py
+++
@@ -1,8 +0,0 @@
-# chat/urls.py
-from django.urls import path
-from .views import ChatSimpleView, ChatStreamView
-
-urlpatterns = [
-    path('chat/', ChatSimpleView.as_view(), name='chat'),           # ← celle qui marche dans le navigateur
-    path('chat/stream/', ChatStreamView.as_view(), name='stream'),
-]
